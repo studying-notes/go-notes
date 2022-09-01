04deb904c451201998e723d68cce72288547b959
Rustle Karl <fu.jiawei@outlook.com>
2022-09-01 14:31:08 +0800

    add: Go 示例测试源码分析

---
+++ src/libraries/standard/testing/example_src/main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func Add(x int, y int) int {
+	return x + y
+}
+
+func main() {
+	fmt.Println("Golang is awesome!")
+}
---
+++ src/libraries/standard/testing/example_src/main_test.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func ExampleAdd() {
+	fmt.Println(Add(1, 2))
+	// Output:
+	// 3
+}
